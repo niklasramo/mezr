78ba040ad9994aa43a400279141abb36b82ca290
Niklas Rämö <inramo@gmail.com>
2023-11-25 15:23:14 +0200

    Update deps.

--- src/getOffset.ts
+++ src/getOffset.ts
@@ -10,15 +10,15 @@ export function getOffset(element: BoxObject, offsetRoot?: BoxObject): BoxOffset
   const offset = isRectObject(element)
     ? { left: element.left, top: element.top }
     : Array.isArray(element)
-    ? getOffsetFromDocument(...element)
-    : getOffsetFromDocument(element);
+      ? getOffsetFromDocument(...element)
+      : getOffsetFromDocument(element);
 
   if (offsetRoot && !isDocument(offsetRoot)) {
     const offsetShift = isRectObject(offsetRoot)
       ? offsetRoot
       : Array.isArray(offsetRoot)
-      ? getOffsetFromDocument(offsetRoot[0], offsetRoot[1])
-      : getOffsetFromDocument(offsetRoot);
+        ? getOffsetFromDocument(offsetRoot[0], offsetRoot[1])
+        : getOffsetFromDocument(offsetRoot);
     offset.left -= offsetShift.left;
     offset.top -= offsetShift.top;
   }
